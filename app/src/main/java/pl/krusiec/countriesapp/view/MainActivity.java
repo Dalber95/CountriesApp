2ac7e54a864d10d3db0e1854f386653afbaaae29
Kamil <krusiec.kamil82@gmail.com>
2020-02-12 17:16:22 +0100

    Implement the view

--- app/src/main/java/pl/krusiec/countriesapp/view/MainActivity.java
+++ app/src/main/java/pl/krusiec/countriesapp/view/MainActivity.java
@@ -1,15 +1,75 @@
 package pl.krusiec.countriesapp.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import pl.krusiec.countriesapp.R;
+import pl.krusiec.countriesapp.viewmodel.ListViewModel;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.countriesList)
+    RecyclerView countriesList;
+
+    @BindView(R.id.listError)
+    TextView listError;
+
+    @BindView(R.id.loadingView)
+    ProgressBar loadingView;
+
+    @BindView(R.id.swipeRefreshLayout)
+    SwipeRefreshLayout refreshLayout;
+
+    private ListViewModel viewModel;
+    private CountryListAdapter adapter = new CountryListAdapter(new ArrayList<>());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        viewModel = ViewModelProviders.of(this).get(ListViewModel.class);
+        viewModel.refresh();
+
+        countriesList.setLayoutManager(new LinearLayoutManager(this));
+        countriesList.setAdapter(adapter);
+
+        observerViewModel();
+    }
+
+    private void observerViewModel() {
+        viewModel.countries.observe(this, countryModels -> {
+            if (countryModels != null) {
+                countriesList.setVisibility(View.VISIBLE);
+                adapter.updateCountries(countryModels);
+            }
+        });
+
+        viewModel.countryLoadError.observe(this, isError -> {
+            listError.setVisibility(isError ? View.VISIBLE : View.GONE);
+        });
+
+        viewModel.loading.observe(this, isLoading -> {
+            if (isLoading != null) {
+                loadingView.setVisibility(isLoading ? View.VISIBLE : View.GONE);
+                if (isLoading) {
+                    listError.setVisibility(View.GONE);
+                    countriesList.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
